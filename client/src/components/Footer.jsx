138bd8d01a1d920f876a87e88d20db629fa494d9
HCook <166083664+Hayden-Cook@users.noreply.github.com>
2024-12-04 20:10:58 -0600

    added the nav bar, header, and footer components

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// This is our Footer component. It will contain the name of the app and the year.
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>&copy; 2024 GamerBud</p>
+            <div></div>
+        </footer>
+    );
+};
+
+export default Footer;
