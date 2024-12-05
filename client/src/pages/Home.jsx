40037b578fc18ec6cdeb26a8a3cc54d0d5a2151e
Ethan Owens <ethan@Ethans-MacBook-Pro.local>
2024-12-04 20:43:03 -0600

    now can seed posts

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-  <p>The Home page works</p>
+  <img src="https://image.api.playstation.com/cdn/UP1018/CUSA00133_00/due3Vp0T2VSGfBtGsWjVnrL4o882iYVk.png"></img>
   )
 };
 
