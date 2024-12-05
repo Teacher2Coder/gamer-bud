51df81de0a1845ca95baa81633bbc1f4776c7b70
shawnperez92 <perez.shawn92@gmail.com>
2024-12-04 19:55:50 -0600

    added welcome h1 to homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,16 @@
+import React from "react";
+
 const Home = () => {
   return (
-  <p>The Home page works</p>
-  )
+    <div style={styles.container}>
+      <header style={styles.header}>
+      <h1 style={styles.title}>Welcome to Gamer Bud!</h1>
+      <p style={styles.subtitle}>Find your next gaming buddy!</p>
+      </header>
+
+      <main style={styles.main}>
+        <section style={styles.catsection}>
+          
 };
 
 export default Home;
