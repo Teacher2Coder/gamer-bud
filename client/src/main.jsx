d0c2a8c78bb7bafcd5bf8d051e2113027f055ea5
james <jamesgreaney40@gmail.com>
2024-12-04 22:15:04 -0500

    edited code

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -21,11 +21,7 @@ const router = createBrowserRouter([
         path: "/login",
         element: <Login />,
       },
-      {
-        path: "/signup",
-        element: <Signup />,
-      },
-    ],
+      ],
   },
 ]);
 
