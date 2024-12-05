4bcd359430704ef58de13154f54223ec30338175
shawnperez92 <perez.shawn92@gmail.com>
2024-12-04 20:25:21 -0600

    basic homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,15 +2,25 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div style={styles.container}>
-      <header style={styles.header}>
-      <h1 style={styles.title}>Welcome to Gamer Bud!</h1>
-      <p style={styles.subtitle}>Find your next gaming buddy!</p>
+    <div>
+      <header>
+      <h1>Welcome to Gamer Bud!</h1>
+      <p>Find your next gaming buddy!</p>
       </header>
 
-      <main style={styles.main}>
-        <section style={styles.catsection}>
-          
+    <div style={styles.container}>
+      <main>
+        <section>
+          <h2>Join or Create a Team</h2>
+          <p>
+            Find a team to join or create your own team to play with others.
+          </p>
+          {/* </section><button style={styles.button}>Join a Team</button> */}
+        </section>
+      </main>
+    </div>
+    </div>
+  );
 };
 
 export default Home;
