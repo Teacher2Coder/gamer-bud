ab83799f89eead0b29c7eb36adb53d4ce63377ce
shawnperez92 <perez.shawn92@gmail.com>
2024-12-05 20:50:09 -0600

    added css to buddysearch.css inline with Home

--- client/src/pages/BuddySearch.jsx
+++ client/src/pages/BuddySearch.jsx
@@ -2,6 +2,7 @@ import { useQuery } from '@apollo/client';
 import PreviewPost from '../components/PreviewPost';
 import { QUERY_POSTS } from '../utils/queries';
 import { Container } from '@chakra-ui/react'
+import './BuddySearch.css';
 
 const BuddySearch = () => {
   const { loading, data } = useQuery(QUERY_POSTS);
