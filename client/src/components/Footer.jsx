72e1a2012828a2f0147378c895432114bf178ad2
HCook <166083664+Hayden-Cook@users.noreply.github.com>
2024-12-05 21:03:42 -0600

    added more title.css code, and adjusted App.jsx and App.css

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,7 +6,7 @@ import '../styles/Footer.css';
 const Footer = () => {
     return (
         <footer className="footer-container">
-            <p>&copy; 2024 GamerBud</p>
+            <p>&copy; 2024 GamerBud. All rights reserved.</p>
             <div></div>
         </footer>
     );
