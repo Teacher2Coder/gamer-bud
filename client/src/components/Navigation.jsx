0edc815c779ee66227a87a1d6f016e487aae1734
HCook <166083664+Hayden-Cook@users.noreply.github.com>
2024-12-11 19:14:05 -0600

    changed the navbar and header css to fit the flow of the website

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-
+import '../styles/Navigation.css';
 
 // This is our Navigation component. We will have all of our page links here so that the user can navigate through the app.
 
 const Navigation = () => {
     return (
-        <nav>
+        <nav className="navbar">
             <ul>
                 <li><NavLink to ="/" end>Home</NavLink></li>
                 <li><NavLink to ="/signup">Sign Up</NavLink></li>
